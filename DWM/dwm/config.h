cbff744aaaf0c0f5b01b0ab2397416672eaf0406
Justin Kosten <justin1122002@gmail.com>
2022-05-13 02:42:13 +0200

    remove sl status

--- DWM/dwm/config.h
+++ DWM/dwm/config.h
@@ -7,12 +7,12 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=17" };
-static const char dmenufont[]       = "monospace:size=15";
+static const char dmenufont[]       = "monospace:size=17";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577"; //#005577 #dc322f
+static const char col_cyan[]        = "#005577"; //#005577 #dc322f c46669
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
