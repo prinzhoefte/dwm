858b5111081c2d4bb61c63bc8d44d5972e63b62f
Justin Kosten <justin1122002@gmail.com>
2022-07-13 00:03:30 +0200

    clean some stuff up and make bar pretty

--- DWM/dwm-bar/bar-functions/dwm_resources.sh
+++ DWM/dwm-bar/bar-functions/dwm_resources.sh
@@ -1,9 +1,3 @@
-#!/bin/sh
-
-# A dwm_bar function to display information regarding system memory, CPU temperature, and storage
-# Joe Standring <git@joestandring.com>
-# GNU GPLv3
-
 dwm_resources () {
 	free_output=$(free -h | grep Mem)
 	MEMUSED=$(echo $free_output | awk '{print $3}')
@@ -12,8 +6,7 @@ dwm_resources () {
 	temp=$(sensors -u coretemp-isa-000 | grep temp1_i | awk '{print $2}')
 	newTemp=${temp%.*}
 
-	printf "  %s| %s| %sC" "$MEMUSED" "$CPU" "$newTemp"
-	printf "|"
+	printf " [ %s] [ %s%%] [ %s°] [ %s]" "$MEMUSED" "$CPU" "$newTemp" "$(date "+%d-%m %T")"
 }
 
 dwm_resources
