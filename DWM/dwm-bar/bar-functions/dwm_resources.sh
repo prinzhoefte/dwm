f183239c3ebb26997d1eb2fa09c8c95fbd5b3ed9
Justin Kosten <justin1122002@gmail.com>
2022-08-25 00:35:26 +0200

    english

--- DWM/dwm-bar/bar-functions/dwm_resources.sh
+++ DWM/dwm-bar/bar-functions/dwm_resources.sh
@@ -5,10 +5,10 @@
 # GNU GPLv3
 
 dwm_resources () {
-	free_output=$(free -h | grep Speicher)
+	free_output=$(free -h | grep Mem)
 	MEMUSED=$(echo $free_output | awk '{print $3}')
 	MEMTOT=$(echo $free_output | awk '{print $2}')
-	CPU=$(top -bn1 | grep CPU | awk 'NR==1{print $2}')
+	CPU=$(top -bn1 | grep Cpu | awk 'NR==1{print $2}')
 	temp=$(sensors -u k10temp-pci-00c3 | grep temp1 | awk '{print $2}')
 	newTemp=${temp%.*}
 
