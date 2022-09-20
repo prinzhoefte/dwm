6a1aaacc3d2d14c159259717113c6c1fbbf4f61a
Justin Kosten <justin1122002@gmail.com>
2022-09-20 09:57:16 +0200

    percentage

--- DWM/dwm-bar/bar-functions/dwm_resources.sh
+++ DWM/dwm-bar/bar-functions/dwm_resources.sh
@@ -8,7 +8,7 @@ dwm_resources () {
 	free_output=$(free -h | grep Mem)
 	MEMUSED=$(echo $free_output | awk '{print $3}')
 	MEMTOT=$(echo $free_output | awk '{print $2}')
-	CPU=$(top -bn1 | grep Cpu | awk 'NR==1{print $2}')
+	CPU=$(top -bn1 | grep Cpu | awk 'NR==1{print $2}')%
 	temp=$(sensors -u k10temp-pci-00c3 | grep temp1 | awk '{print $2}')
 	newTemp=${temp%.*}
 
