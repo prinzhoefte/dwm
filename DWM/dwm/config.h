3e70a571110afb7bd13f31830c0252f81d8d9d48
Justin Kosten <justin1122002@gmail.com>
2022-11-16 16:28:13 +0100

    wallpapers into repo

--- DWM/dwm/config.h
+++ DWM/dwm/config.h
@@ -30,7 +30,7 @@ static const char *const autostart[] = {
 	"sh", "-c", "autorandr -c;", NULL,
 	"sh", "-c", "/home/justin/code/dwm/DWM/dwm-bar/dwm_bar.sh", NULL,
 	//"sh", "-c", "/home/justin/code/dwm/DWM/animatedWallpaper/bg.sh", NULL,			//Animated Wallpaper
-	"sh", "-c", "sleep 2; feh --bg-scale /home/justin/Bilder/mountain.jpg", NULL, //Normal Wallpaper
+	"sh", "-c", "sleep 2; feh --bg-scale /home/justin/code/dwm/DWM/wallpapers/red.jpg", NULL, //Normal Wallpaper
 	"dunst", NULL,
 	"xcompmgr", "-c", NULL,
 	NULL
