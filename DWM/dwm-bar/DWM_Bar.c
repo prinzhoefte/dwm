3b6f3e10e8a5d225fd5c774a6c0f0edbe3855aab
Justin Rauch <justin.rauch@rheinmetall.com>
2023-06-12 11:24:25 +0200

    added wifi to bar

--- DWM/dwm-bar/DWM_Bar.c
+++ DWM/dwm-bar/DWM_Bar.c
@@ -8,51 +8,67 @@
 #include <stdlib.h>
 #include <string.h>
 
-void removeLast(char* str) {
+void
+removeLast(char* str) {
     str[strlen(str)-1] = '\0';
 }
 
-void getCpu(char* cpu) {
+void
+getNetwork(char* network) {
+	FILE *f = popen("nmcli | grep 'connected' | awk 'NR==1{print $4}'", "r");
+    fgets(network, 100, f);
+    removeLast(network);
+	pclose(f);
+}
+
+void
+getCpu(char* cpu) {
     FILE *f = popen("top -bn1 | grep Cpu | awk 'NR==1{print $2}'", "r");
     fgets(cpu, 6, f);
     removeLast(cpu);
     pclose(f);
 }
 
-void getMemory(char* memory) {
+void
+getMemory(char* memory) {
     FILE *f = popen("free -h | grep Mem | awk '{print $3}'", "r");
     fgets(memory, 6, f);
     removeLast(memory);
     pclose(f);
 }
 
-void getTemp(char* temp) {
+void
+getTemp(char* temp) {
     FILE *f = popen("sensors -u k10temp-pci-00c3 | grep temp1 | awk '{print $2}'", "r");
     fgets(temp, 4, f);
     removeLast(temp);
     pclose(f);
 }
 
-void getCharge(char* charge) {
+void
+getCharge(char* charge) {
     FILE *f = popen("cat /sys/class/power_supply/BAT0/capacity", "r");
     fgets(charge, 5, f);
     removeLast(charge);
     pclose(f);
 }
 
-void getDate(char* date) {
+void
+getDate(char* date) {
     FILE *f = popen("date \"+%d-%m %T\"", "r");
     fgets(date, 15, f);
     pclose(f);
 }
 
-int main() {
-    char* output = malloc(sizeof(char));
-    char* cpu = malloc(sizeof(char));
-    char* memory = malloc(sizeof(char));
-    char* temp = malloc(sizeof(char));
-    char* charge = malloc(sizeof(char));
-    char* date = malloc(sizeof(char));
+int
+main() {
+    char* output = malloc(sizeof(char)*136);
+    char* cpu = malloc(sizeof(char)*6);
+    char* memory = malloc(sizeof(char)*6);
+    char* temp = malloc(sizeof(char)*4);
+    char* charge = malloc(sizeof(char)*5);
+    char* date = malloc(sizeof(char)*15);
+    char* network = malloc(sizeof(char)*100);
 
     while(1) {
         getCpu(cpu);
@@ -60,9 +76,11 @@ int main() {
         getTemp(temp);
         getCharge(charge);
         getDate(date);
+        getNetwork(network);
         sprintf(output, 
-            "%s '[ %s%] [ %s] [ %sC] [ %s%] [ %s]'", 
+            "%s '[ %s] [ %s%] [ %s] [ %sC] [ %s%] [ %s]'", 
             "xsetroot -name ", 
+            network,
             cpu, 
             memory, 
             temp,
